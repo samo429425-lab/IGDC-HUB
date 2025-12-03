854c09dde49061dc13add3a77b1140ca1f15a679
samo429425-lab <samo429425@gmail.com>
2025-12-04 01:28:11 +0900

    Delete netlify/functions directory

--- netlify/functions/ig-oembed.js
+++
@@ -1,7 +0,0 @@
-
-// /.netlify/functions/ig-oembed (compat)
-// Delegate to /oembed for unified handling.
-exports.handler = async (event, ctx) => {
-  const { handler } = require('./oembed.js');
-  return handler(event, ctx);
-};
